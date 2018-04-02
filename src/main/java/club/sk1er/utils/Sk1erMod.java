58ee425c327274d2535d56fa6d37df0b71334061
SimplyRin <simplyrin.dev@gmail.com>
2018-04-02 19:06:11 +0900

    Update Sk1erMod.java

--- src/main/java/club/sk1er/utils/Sk1erMod.java
+++ src/main/java/club/sk1er/utils/Sk1erMod.java
@@ -1,6 +1,7 @@
 package club.sk1er.utils;
 
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -10,16 +11,37 @@ import org.apache.commons.io.IOUtils;
 public class Sk1erMod {
 
 	public static String rawWithAgent(String url) {
+		return rawWithAgent(url, null);
+	}
+
+	public static String rawWithAgent(String url, String args) {
+		return rawWithAgent(url, args, "Mozilla/4.77");
+	}
+
+	public static String rawWithAgent(String url, String args, String userAgent) {
 		System.out.println("Fetching: " + url);
+
 		try {
 			URL u = new URL(url);
 			HttpURLConnection connection = (HttpURLConnection) u.openConnection();
-			connection.setRequestMethod("GET");
+			if(args != null) {
+				connection.setRequestMethod("POST");
+			} else {
+				connection.setRequestMethod("GET");
+			}
 			connection.setUseCaches(true);
-			connection.addRequestProperty("User-Agent", "Mozilla/4.76");
+			connection.addRequestProperty("User-Agent", userAgent);
 			connection.setReadTimeout(15000);
 			connection.setConnectTimeout(15000);
 			connection.setDoOutput(true);
+
+			if(args != null) {
+				OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+				out.write(args);
+				out.close();
+				connection.connect();
+			}
+
 			InputStream is = connection.getInputStream();
 			Charset encoding = Charset.defaultCharset();
 			String s = IOUtils.toString(is, encoding);
